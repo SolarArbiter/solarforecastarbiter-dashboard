e079ea4b07384b382988b5c4e9a82012b2d9276b
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-04-03 10:37:07 -0700

    add blueprint to authenticate with Auth0

---
+++ sfa_dash/blueprints/auth0.py
@@ -0,0 +1,46 @@
+from functools import partial
+
+
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
+from flask.globals import LocalProxy, _lookup_app_object
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+
+
+def make_auth0_blueprint(
+        base_url, client_id=None, client_secret=None,
+        scope=None, redirect_url=None,
+        redirect_to=None, login_url=None,
+        session_class=None,
+        storage=None):
+    scope = scope or ['openid', 'email', 'profile', 'offline_access']
+    auth0_bp = OAuth2ConsumerBlueprint(
+        'auth0', __name__,
+        client_id=client_id,
+        client_secret=client_secret,
+        base_url=base_url,
+        token_url=f'{base_url}/oauth/token',
+        authorization_url=f'{base_url}/authorize',
+        authorization_url_params={
+            'audience': 'https://api.solarforecastarbiter.org'},
+        redirect_url=redirect_url,
+        redirect_to=redirect_to,
+        scope=scope,
+        login_url=login_url,
+        session_class=session_class,
+        storage=storage,
+        )
+    auth0_bp.from_config['client_id'] = 'AUTH0_OAUTH_CLIENT_ID'
+    auth0_bp.from_config['client_secret'] = 'AUTH0_OAUTH_CLIENT_SECRET'
+
+    @auth0_bp.before_app_request
+    def set_applocal_session():
+        ctx = stack.top
+        ctx.auth0_oauth = auth0_bp.session
+
+    return auth0_bp
+
+
+auth0 = LocalProxy(partial(_lookup_app_object, 'auth0_oauth'))
