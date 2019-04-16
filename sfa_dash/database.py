4b4e3dc1fd5a5676b9f62ef608742c2a843ee668
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-04-16 15:17:48 -0700

    add sqlalchemy_utils back as undeclared dependency of flask-dance

--- sfa_dash/database.py
+++ sfa_dash/database.py
@@ -1,32 +1,16 @@
 import datetime as dt
-import json
 
 
 from flask_dance.consumer.storage.sqla import SQLAlchemyStorage
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.types import TypeDecorator, TEXT
+from sqlalchemy_utils import JSONType
 
 
 from sfa_dash.blueprints.auth0 import current_user
 
 
 db = SQLAlchemy()
-
-
-class JSONEncodedDict(TypeDecorator):
-    """Encode JSON as TEXT"""
-    impl = TEXT
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            value = json.dumps(value)
-        return value
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            value = json.loads(value)
-        return value
 
 
 class OAuth(db.Model):
@@ -38,7 +22,7 @@ class OAuth(db.Model):
     provider = db.Column(db.String(24), nullable=False)
     created_at = db.Column(db.DateTime, default=dt.datetime.utcnow,
                            nullable=False)
-    token = db.Column(JSONEncodedDict, nullable=False)
+    token = db.Column(JSONType, nullable=False)
 
 
 session_storage = SQLAlchemyStorage(OAuth, db.session,
