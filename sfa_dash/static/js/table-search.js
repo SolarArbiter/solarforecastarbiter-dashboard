d12516c259216956fb879be542e1166dc9100287
Leland Boeman <lboeman@email.arizona.edu>
2019-06-06 12:43:05 -0700

    check for a value in searchbox on document load and fire a keyup event

--- sfa_dash/static/js/table-search.js
+++ sfa_dash/static/js/table-search.js
@@ -6,24 +6,29 @@ $(document).ready(function() {
     var listItem = $('.results tbody').children('tr');
     var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
     
-  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
-        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-    }
-  });
-    
-  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
-    $(this).attr('visible','false');
-  });
+    $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+          return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+      }
+    });
+      
+    $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+      $(this).attr('visible','false');
+    });
 
-  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
-    $(this).attr('visible','true');
-  });
+    $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+      $(this).attr('visible','true');
+    });
 
-  var jobCount = $('.results tbody tr[visible="true"]').length;
+    var jobCount = $('.results tbody tr[visible="true"]').length;
     $('.counter').text(jobCount + ' item');
 
-  if(jobCount == '0') {$('.no-result').show();}
+  
+    if(jobCount == '0') {$('.no-result').show();}
     else {$('.no-result').hide();}
-		  });
+  });
+
+  if ($(".search").val() != null){
+      $(".search").keyup();
+  }
 
 });
