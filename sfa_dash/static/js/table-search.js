cf54b4cb00a1a8d9ac4378249fb0508cc8fe86d2
Leland Boeman <lboeman@email.arizona.edu>
2019-06-07 09:30:30 -0700

    reorganize and comment

--- sfa_dash/static/js/table-search.js
+++ sfa_dash/static/js/table-search.js
@@ -1,6 +1,6 @@
 /* Jquery for filtering and searching tables.
  * Defines filter functions that return a jquery object list of items that need to be hidden
- * from the table. The functions should be registered in the `toHideFns` list to be called by
+ * from the table. The functions should be added to the `toHideFns` array to be called by
  * callback functions. At the end of the file, your widgets should have their callback set to
  * the `applyTableFilters` function
  *
@@ -13,21 +13,28 @@ $(document).ready(function() {
             return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
         }
     });
-    
-    // list of functions to use to build the list of rows to hide
-    var toHideFns = []
 
+    /*
+     * Filter functions
+     *
+     * Check some state within the dom, and return a jquery object list of elements
+     * to hide.
+     */
     function searchTable() {
-        /* Reads the value of the search input and creates a list of rows to hide
+        /* 
+         * Reads the value of the search input and creates a list of rows to hide
          * based on which rows do not contain the search term
          */
         var searchTerm = $(".search").val();
         var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
         return $(".results tbody tr").not(":containsi('" + searchSplit + "')")
     }
-    toHideFns.push(searchTable);
-
     function filteredOrgs() {
+        /* 
+         * Iterates through the `.org-filter-option` checkboxes, for each unchecked box,
+         * builds a list of tr elements whose `.provider-column` fields contain the
+         * checkboxes value and returns the concatenated list.
+         */
         orgsToHide = $([]);
         $('.org-filter-option').each(function (e){
             if (!this.checked){
@@ -36,26 +43,15 @@ $(document).ready(function() {
         });
         return orgsToHide
     }
-    if ($('#provider-header').length){
-        // check for the id of the table header for the provider(organization field)
-        // and insert checkboxes to hide/display the table rows
-        var availableOrgs = new Set([]);
-        var orgs = $(".provider-column")
-        for (i = 0; i < orgs.length; i++) {
-            availableOrgs.add(orgs[i].textContent);
-        }
-        var filter_div = $("<div id='org-filters' class='collapse'>Filter by Organization <a href='#' role='button' id='org-filter-collapse'></a><br/><hr><ul class='org-filter-options'></ul></div>");
-        filter_div.appendTo("#provider-header");
-        availableOrgs.forEach(function (e) {$(".org-filter-options").append(`<li><input class="org-filter-option"value="${e}" type="checkbox" checked>${e}</li>`)});
-        $('#org-filter-collapse').click(function() {
-            $('#org-filters').collapse('toggle');
-        });
-        toHideFns.push(filteredOrgs);
-    }
-   
+    
+    /*
+     * Filter application functions
+     */
+    var toHideFns = []
     function allHiddenElements(){
-        /* Returns a JQuery object list of rows that need to be hidden
-         * concatenating the results of calls to the toHideFns
+        /* 
+         * Returns a JQuery object list of rows that need to be hidden
+         * by concatenating the results of calls to the toHideFns.
          */
         elements = $([]);
         toHideFns.forEach(function(e){
@@ -64,7 +60,8 @@ $(document).ready(function() {
         return elements
     }
     function applyTableFilters() {
-        /* Gets a list of table rows, and then builds a list of all the
+        /* 
+         * Gets a list of table rows, and then builds a list of all the
          * rows to be excluded due to filters, removes the rows to hide
          * from all rows, sets the remainder to visible and hides the
          * others.
@@ -79,14 +76,34 @@ $(document).ready(function() {
         if(jobCount == '0') {$('.no-result').show();}
         else {$('.no-result').hide();}
     }
+
+
+    toHideFns.push(searchTable);
+    /* 
+     * If the Provider header id exists, build a dropdown ul of checkboxes from the
+     * list of organizations in the table.
+     */
+    if ($('#provider-header').length){
+        var availableOrgs = new Set([]);
+        var orgs = $(".provider-column")
+        for (i = 0; i < orgs.length; i++) {
+            availableOrgs.add(orgs[i].textContent);
+        }
+        var filter_div = $("<div id='org-filters' class='collapse'>Filter by Organization <a href='#' role='button' id='org-filter-collapse'></a><br/><hr><ul class='org-filter-options'></ul></div>");
+        filter_div.appendTo("#provider-header");
+        availableOrgs.forEach(function (e) {$(".org-filter-options").append(`<li><input class="org-filter-option"value="${e}" type="checkbox" checked>${e}</li>`)});
+        $('#org-filter-collapse').click(function() {
+            $('#org-filters').collapse('toggle');
+        });
+        toHideFns.push(filteredOrgs);
+    }
     
     /*
-     * Register dom element callbacks
+     * Register DOM element callbacks
      */
     $(".search").keyup(applyTableFilters);
     if ($(".search").val() != null){
-        // on page load, check for values in the search field.
-        // if they exist, run the filtering function
+        // if searchbar is filled on page load, apply filters
         applyTableFilters();
     }
     $(".org-filter-option").change(applyTableFilters);
