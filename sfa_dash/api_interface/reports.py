fa0d93452442fabf16c87d75dcb083b98acf31fd
Leland Boeman <lboeman@email.arizona.edu>
2019-06-27 17:00:09 -0700

    report generation and api integration

--- sfa_dash/api_interface/reports.py
+++ sfa_dash/api_interface/reports.py
@@ -1,24 +1,52 @@
-from sfa_dash.api_interface import get_request, post_request, delete_request
+from flask import abort
+from flask import current_app as app
+from requests.exceptions import HTTPError
+
+from sfa_dash import oauth_request_session
+from solarforecastarbiter.io.api import APISession
+
+
+def _get_session():
+    token = oauth_request_session.token['access_token']
+    session = APISession(token, base_url=app.config['SFA_API_URL'])
+    return session
 
 
 def get_metadata(report_id):
-    req = get_request(f'/reports/{report_id}')
-    return req
+    try:
+        session = _get_session()
+        report = session.get_report(report_id)
+    except HTTPError:
+        abort(404)
+    return report
 
 
-def list_metadata(site_id=None):
-    if site_id is not None:
-        req = get_request(f'/sites/{site_id}/reports')
+def list_full_reports():
+    try:
+        session = _get_session()
+        report = session.get('/reports/')
+    except HTTPError:
+        abort(404)
     else:
-        req = get_request('/reports/')
-    return req
+        return report.json()
+
+
+def list_metadata():
+    try:
+        session = _get_session()
+        reports = session.list_reports()
+    except HTTPError:
+        abort(404)
+    return reports
 
 
 def post_metadata(report_dict):
-    req = post_request('/reports/', report_dict)
-    return req
+    session = _get_session()
+    post_request = session.post('/reports/', json=report_dict)
+    return post_request
 
 
 def delete(report_id):
-    req = delete_request(f'/reports/{report_id}')
-    return req
+    session = _get_session()
+    delete = session.delete(f'/reports/{report_id}')
+    return delete
--- sfa_dash/blueprints/reports.py
+++ sfa_dash/blueprints/reports.py
@@ -1,13 +1,11 @@
 """Draft of reports endpoints/pages. Need to integrate core report generation.
 """
 import json
-from flask import request, redirect, url_for, abort
+from flask import request, redirect, url_for, abort, render_template
 import pandas as pd
 from requests.exceptions import HTTPError
 
-from solarforecastarbiter.io.api import APISession
 from solarforecastarbiter.reports.main import report_to_html_body
-from sfa_dash import oauth_request_session
 from sfa_dash.api_interface import observations, forecasts, reports
 from sfa_dash.blueprints.base import BaseView
 
@@ -16,8 +14,7 @@ class ReportsView(BaseView):
     template = 'dash/reports.html'
 
     def template_args(self):
-        reports_req = reports.list_metadata()
-        reports_list = reports_req.json()
+        reports_list = reports.list_full_reports()
         return {
             "page_title": 'Reports',
             "reports": reports_list,
@@ -27,15 +24,6 @@ class ReportsView(BaseView):
 class ReportForm(BaseView):
     template = 'forms/report_form.html'
 
-    def parse_extra_parameters(self, with_params):
-        try:
-            params = json.loads(with_params['extra_parameters'])
-        except json.decoder.JSONDecodeError:
-            params = {}
-        with_params.update({
-            'extra_parameters': params,
-        })
-
     def get_pairable_objects(self):
         """Requests the forecasts and observations from
         the api for injecting into the dom as a js variable
@@ -44,10 +32,10 @@ class ReportForm(BaseView):
         forecast_request = forecasts.list_metadata()
         observation_list = observation_request.json()
         for obs in observation_list:
-            self.parse_extra_parameters(obs)
+            del obs['extra_parameters']
         forecast_list = forecast_request.json()
         for fx in forecast_list:
-            self.parse_extra_parameters(fx)
+            del fx['extra_parameters']
         return {
             'observations': observation_list,
             'forecasts': forecast_list,
@@ -69,12 +57,12 @@ class ReportForm(BaseView):
 
     def zip_object_pairs(self, form_data):
         """Create a list of observation, forecast tuples from the the
-        (observation-n, forecast-n) input elements inserted by
+        (forecast-n, observation-n) input elements inserted by
         report-handling.js
         """
-        obs = self.field_values('observation-', form_data)
         fx = self.field_values('forecast-', form_data)
-        pairs = list(zip(obs, fx))
+        obs = self.field_values('observation-', form_data)
+        pairs = list(zip(fx, obs))
         return pairs
 
     def parse_metrics(self, form_data):
@@ -82,7 +70,7 @@ class ReportForm(BaseView):
         attribute of metrics. These elements are checkbox inputs, and are only
         included in the form data when selected.
         """
-        return [k for k, v in form_data.items() if v == 'metrics']
+        return [k.lower() for k, v in form_data.items() if v == 'metrics']
 
     def parse_filters(self, form_data):
         """Return an empty array until we know more about how we want
@@ -118,31 +106,79 @@ class ReportForm(BaseView):
     def post(self):
         form_data = request.form
         api_payload = self.report_formatter(form_data)
-        post_req = reports.post_metadata(api_payload)
-        if post_req.status_code == 201:
-            return redirect(url_for('data_dashboard.reports'))
-        elif post_req.status_code == 400:
-            # flatten error response to handle nesting
-            errors = post_req.json()['errors']
+        try:
+            reports.post_metadata(api_payload)
+        except HTTPError as e:
+            if e.response.status_code == 400:
+                # flatten error response to handle nesting
+                errors = e.response.json()['errors']
+                return super().get(form_data=form_data, errors=errors)
+            elif e.response.status_code == 404:
+                errors = {'error': ['Permission to create report denied.']}
+            else:
+                errors = {'error': ['An unrecoverable error occured.']}
             return super().get(form_data=form_data, errors=errors)
-        elif post_req.status_code == 404:
-            errors = {'error': ['Permission to create report denied.']}
-        else:
-            errors = {'error': ['An unrecoverable error occured.']}
-        return super().get(form_data=form_data, errors=errors)
+        return redirect(url_for(
+            'data_dashboard.reports',
+            messages={'creation': 'successful'}))
 
 
 class ReportView(BaseView):
     template = 'data/report.html'
 
     def template_args(self):
-        return {'report': report_to_html_body(self.metadata)}
+        report_template = report_to_html_body(self.metadata)
+        return {'report': report_template,
+                'bokeh_script': ''}
 
     def get(self, uuid):
-        token = oauth_request_session.token['access_token']
-        session = APISession(token)
-        try:
-            report_request = session.get_report(uuid)
-        except HTTPError as e:
-            abort(404);
+        self.metadata = reports.get_metadata(uuid)
         return super().get()
+
+
+class DeleteReportView(BaseView):
+    template = 'forms/deletion_form.html'
+    metadata_template = 'data/metadata/report_metadata.html'
+
+    def template_args(self):
+        return {
+            'data_type': 'report',
+            'uuid': self.metadata.report_id,
+            'metadata': render_template(
+                self.metadata_template,
+                data_type='Report',
+                metadata_object=self.metadata
+            ),
+        }
+
+    def get(self, uuid, **kwargs):
+        self.metadata = reports.get_metadata(uuid)
+        return super().get(**kwargs)
+
+    def post(self, uuid):
+        confirmation_url = url_for(f'data_dashboard.delete_report',
+                                   _external=True,
+                                   uuid=uuid)
+        if request.headers['Referer'] != confirmation_url:
+            # If the user was directed from anywhere other than
+            # the confirmation page, redirect to confirm.
+            return redirect(confirmation_url)
+        try:
+            delete_request = reports.delete(uuid)
+        except HTTPError as e:
+            if e.response.status_code == 400:
+                # Redirect and display errors if the delete request
+                # failed
+                response_json = delete_request.json()
+                errors = response_json['errors']
+                return self.get(uuid, errors=errors)
+            elif e.response.status_code == 404:
+                abort(404)
+            else:
+                errors = {
+                    "error": ["Could not complete the requested action."]
+                }
+                return self.get(uuid, errors=errors)
+        return redirect(url_for(
+            f'data_dashboard.reports',
+            messages={'delete': ['Success']}))
