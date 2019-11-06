496338c7b1727d068fffff868a67b6aef9f2dfe7
Leland Boeman <lboeman@email.arizona.edu>
2019-11-06 11:09:58 -0700

    move general functions to DataDashView, correct cdf_forecast_group breadcrumbs

--- sfa_dash/blueprints/dash.py
+++ sfa_dash/blueprints/dash.py
@@ -21,6 +21,72 @@ class DataDashView(BaseView):
     def get_site_metadata(self, site_id):
         return handle_response(sites.get_metadata(site_id))
 
+    def set_site_or_aggregate_metadata(self):
+        """Searches for a site_id or aggregate_id  in self.metadata
+        and loads the expected metadata object from the api in either
+        the 'site' or 'aggregate' key. If the object could not be retrieved,
+        sets a warning, sets the 'plot' template argument to None and
+        reraises the DataRequestError.
+        """
+        if self.metadata['site_id'] is not None:
+            try:
+                self.metadata['site'] = self.get_site_metadata(
+                    self.metadata['site_id'])
+            except DataRequestException:
+                self.temp_args.update({
+                    'warnings': {
+                        'Site Access': [
+                            'Site inaccessible. Plots will not be displayed.']
+                    },
+                })
+                raise
+        else:
+            try:
+                self.metadata['aggregate'] = handle_response(
+                    aggregates.get_metadata(self.metadata['aggregate_id']))
+            except DataRequestException:
+                self.temp_args.update({
+                    'warnings': {
+                        'Aggregate Access': [
+                            'Aggregate inaccessible. Plots will not be '
+                            'displayed.']
+                    },
+                })
+                raise
+
+    def set_site_or_aggregate_link(self):
+        """Creates a url to the forecast's site or aggregate. For injection
+        into the metadata template.
+
+        Returns
+        -------
+        link_html: str
+            An anchor tag referencing the site/aggregates page on the
+            dashboard or a string uuid if the metadata wasn't not available,
+            indicating that the current user does not have access to the
+            site or aggregate.
+        """
+        if self.metadata['site_id'] is not None:
+            if self.metadata.get('site') is not None:
+                site_metadata = self.metadata.get('site')
+                site_name = site_metadata['name']
+                site_id = site_metadata['site_id']
+                site_href = url_for('data_dashboard.site_view',
+                                    uuid=site_id)
+                link_html = f'<a href="{site_href}">{site_name}</a>'
+            else:
+                link_html = self.metadata['site_id']
+        elif self.metadata['aggregate_id'] is not None:
+            if self.metadata.get('aggregate') is not None:
+                aggregate_metadata = self.metadata.get('aggregate')
+                aggregate_name = aggregate_metadata['name']
+                aggregate_id = aggregate_metadata['aggregate_id']
+                aggregate_href = url_for('data_dashboard.aggregate_view',
+                                         uuid=aggregate_id)
+                link_html = f'<a href="{aggregate_href}">{aggregate_name}</a>'
+            else:
+                link_html = self.metadata['aggregate_id']
+        self.metadata['location_link'] = link_html
     def parse_start_end_from_querystring(self):
         """Attempts to find the start and end query parameters. If not found,
         returns defaults spanning the last three days. Used for setting
--- sfa_dash/blueprints/main.py
+++ sfa_dash/blueprints/main.py
@@ -275,73 +275,6 @@ class SingleForecastObjectView(DataDashView):
                     'bokeh_script': script_plot[0]
                 })
 
-    def set_site_or_aggregate_metadata(self):
-        """Searches for a site_id or aggregate_id  in self.metadata
-        and loads the expected metadata object from the api in either
-        the 'site' or 'aggregate' key. If the object could not be retrieved,
-        sets a warning, sets the 'plot' template argument to None and
-        reraises the DataRequestError.
-        """
-        if self.metadata['site_id'] is not None:
-            try:
-                self.metadata['site'] = self.get_site_metadata(
-                    self.metadata['site_id'])
-            except DataRequestException:
-                self.temp_args.update({
-                    'warnings': {
-                        'Site Access': [
-                            'Site inaccessible. Plots will not be displayed.']
-                    },
-                })
-                raise
-        else:
-            try:
-                self.metadata['aggregate'] = handle_response(
-                    aggregates.get_metadata(self.metadata['aggregate_id']))
-            except DataRequestException:
-                self.temp_args.update({
-                    'warnings': {
-                        'Aggregate Access': [
-                            'Aggregate inaccessible. Plots will not be '
-                            'displayed.']
-                    },
-                })
-                raise
-
-    def set_site_or_aggregate_link(self):
-        """Creates a url to the forecast's site or aggregate. For injection
-        into the metadata template.
-
-        Returns
-        -------
-        link_html: str
-            An anchor tag referencing the site/aggregates page on the
-            dashboard or a string uuid if the metadata wasn't not available,
-            indicating that the current user does not have access to the
-            site or aggregate.
-        """
-        if self.metadata['site_id'] is not None:
-            if self.metadata.get('site') is not None:
-                site_metadata = self.metadata.get('site')
-                site_name = site_metadata['name']
-                site_id = site_metadata['site_id']
-                site_href = url_for('data_dashboard.site_view',
-                                    uuid=site_id)
-                link_html = f'<a href="{site_href}">{site_name}</a>'
-            else:
-                link_html = self.metadata['site_id']
-        elif self.metadata['aggregate_id'] is not None:
-            if self.metadata.get('aggregate') is not None:
-                aggregate_metadata = self.metadata.get('aggregate')
-                aggregate_name = aggregate_metadata['name']
-                aggregate_id = aggregate_metadata['aggregate_id']
-                aggregate_href = url_for('data_dashboard.aggregate_view',
-                                         uuid=aggregate_id)
-                link_html = f'<a href="{aggregate_href}">{aggregate_name}</a>'
-            else:
-                link_html = self.metadata['aggregate_id']
-        self.metadata['location_link'] = link_html
-
     def get(self, uuid, **kwargs):
         self.temp_args = {}
         start, end = self.parse_start_end_from_querystring()
@@ -390,6 +323,15 @@ class SingleCDFForecastGroupView(SingleForecastObjectView):
             breadcrumb_dict[f'{self.human_label}s'] = url_for(
                 'data_dashboard.cdf_forecast_groups',
                 uuid=self.metadata['site_id'])
+        elif self.metadata.get('aggregate') is not None:
+            breadcrumb_dict['Aggregates'] = url_for(
+                'data_dashboard.aggregates')
+            breadcrumb_dict[self.metadata['aggregate']['name']] = url_for(
+                'data_dashboard.aggregate_view',
+                uuid=self.metadata['aggregate_id'])
+            breadcrumb_dict[f'{self.human_label}s'] = url_for(
+                f'data_dashboard.cdf_forecast_groups',
+                uuid=self.metadata['aggregate_id'])
         else:
             breadcrumb_dict[f'{self.human_label}s'] = url_for(
                 'data_dashboard.cdf_forecast_groups')
