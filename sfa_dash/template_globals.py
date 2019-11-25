ca5fff37af56cb75a97242000fa4097f4c9fc1c2
Leland Boeman <lboeman@email.arizona.edu>
2019-11-25 15:34:58 -0700

    supply metrics/categories as filters for templateing [skip ci]

--- sfa_dash/template_globals.py
+++ sfa_dash/template_globals.py
@@ -6,6 +6,8 @@ import pytz
 
 import sfa_dash
 from sfa_dash import filters
+from solarforecastarbiter.metrics.calculator import AVAILABLE_CATEGORIES
+from solarforecastarbiter.metrics.deterministic import _MAP
 
 TIMEZONES = pytz.country_timezones('US') + list(
     filter(lambda x: 'GMT' in x, pytz.all_timezones))
@@ -16,10 +18,14 @@ VARIABLE_OPTIONS = {key: f'{value} ({filters.api_varname_to_units(key)})'
 
 TIMEZONE_OPTIONS = {tz: tz.replace('_', ' ') for tz in TIMEZONES}
 
+DETERMINISTIC_METRICS = list(_MAP.keys())
+
 
 def template_variables():
     return {
         'dashboard_version': sfa_dash.__version__,
         'variable_options': VARIABLE_OPTIONS,
         'timezone_options': TIMEZONE_OPTIONS,
+        'metric_categories': AVAILABLE_CATEGORIES,
+        'deterministic_metrics': DETERMINISTIC_METRICS,
     }
