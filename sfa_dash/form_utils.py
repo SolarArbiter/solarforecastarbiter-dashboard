d46a6c73727cfbb168c40fca3cf72f49da92baa0
Leland Boeman <lboeman@email.arizona.edu>
2019-12-20 11:49:03 -0700

    form utils header comment

---
+++ sfa_dash/form_utils.py
@@ -0,0 +1,4 @@
+"""Utility functions for converting to and from dictionaries to form data. This
+will allow parsing user posted data into an API request body, as well as
+converting an API response into a dict for pre-filling form data. 
+"""
