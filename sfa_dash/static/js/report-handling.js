5907a4efc10424bfb9d51774690e9ba1f9444432
Leland Boeman <lboeman@email.arizona.edu>
2020-04-02 18:18:53 -0700

    limiting variable by units after first pair selection, handle no pair error and forecast skill no ref error

--- sfa_dash/static/js/report-handling.js
+++ sfa_dash/static/js/report-handling.js
@@ -2,6 +2,55 @@
  *  Creates inputs for defining observation, forecast pairs for a report.
  */
 $(document).ready(function() {
+    var variable_unit_map = {
+        'air_temperature': 'degC',
+        'wind_speed': 'm/s',
+        'ghi': 'W/m^2',
+        'dni': 'W/m^2',
+        'dhi': 'W/m^2',
+        'poa_global': 'W/m^2',
+        'relative_humidity': '%',
+        'ac_power': 'MW',
+        'dc_power': 'MW',
+        'availability': '%',
+        'curtailment': 'MW',
+    }
+    var current_units = null;
+    
+    function unset_units(){
+        /* Set units to null when the last pair is removed */
+        current_units = null;
+        setVariables()
+    }
+    function set_units(variable){
+        console.log('setting units with var ', variable);
+        units = variable_unit_map[variable];
+        if(units){
+            current_units = units;
+        }
+        setVariables();
+    }
+
+    function setVariables(){
+        variable_options = $('#variable-select option');
+        variable_options.removeAttr('hidden');
+        variable_options.removeAttr('disabled');
+        if (current_units){
+            variable_options.each(function(){
+                units = variable_unit_map[$(this).attr('value')]
+                if(units != current_units){
+                    $(this).attr('hidden', true);
+                    $(this).attr('disabled', true);
+                }
+            });
+        }
+        $('#variable-select').val(variable_options.filter(":not([hidden])").val());
+    }
+    function changeVariable(){
+        setVariables(); 
+        filterForecasts();
+    }
+
     function registerDatetimeValidator(input_name){
         /*
          * Applies a regex validator to ensure ISO8601 compliance. This is however, very strict. We
@@ -38,7 +87,10 @@ $(document).ready(function() {
         }
         variables = new Set();
         for (fx in page_data['forecasts']){
-            variables.add(page_data['forecasts'][fx].variable);
+            var new_var = page_data['forecasts'][fx].variable;
+            if (!current_units || variable_unit_map[new_var] == current_units){
+                variables.add(page_data['forecasts'][fx].variable);
+            }
         }
         variable_select = $('<select id="variable-select" class="form-control half-width"><option selected value>All Variables</option></select>');
         variables.forEach(function(variable){
@@ -62,7 +114,14 @@ $(document).ready(function() {
         return $(selectSelector + " option").slice(offset).not(":containsi('" + searchSplit + "')");
     }
 
-    function addPair(truthType, truthName, truthId, fxName, fxId){
+    function applyFxDependentFilters(){
+        filterObservations();
+        filterAggregates();
+        filterReferenceForecasts();
+    }
+
+    function addPair(truthType, truthName, truthId, fxName, fxId,
+                     ref_fxName, ref_fxId){
         /*
          * Returns a Jquery object containing 5 input elements representing a forecast,
          * observation pair:
@@ -76,39 +135,41 @@ $(document).ready(function() {
          */
         var new_object_pair = $(`<div class="object-pair object-pair-${pair_index}">
                 <div class="input-wrapper">
-                  <div class="col-md-6">
-                    <input type="text" class="form-control forecast-field" name="forecast-name-${pair_index}" required disabled value="${fxName}"/>
-                    <input type="hidden" class="form-control forecast-field" name="forecast-id-${pair_index}" required value="${fxId}"/>
-                  </div>
-                  <div class="col-md-6">
-                    <input type="text" class="form-control truth-field" name="truth-name-${pair_index}"  required disabled value="${truthName}"/>
-                    <input type="hidden" class="form-control truth-field" name="truth-id-${pair_index}" required value="${truthId}"/>
-                    <input type="hidden" class="form-control truth-field" name="truth-type-${pair_index}" required value="${truthType}"/>
+                  <div class="col-md-12">
+                    <div class="object-pair-label forecast-name-${pair_index}"><b>Forecast: </b>${fxName}</div>
+                    <input type="hidden" class="form-control forecast-value" name="forecast-id-${pair_index}" required value="${fxId}"/>
+                    <div class="object-pair-label truth-name-${pair_index}"><b>Observation: </b> ${truthName}</div>
+                    <input type="hidden" class="form-control truth-value" name="truth-id-${pair_index}" required value="${truthId}"/>
+                    <input type="hidden" class="form-control truth-type-value" name="truth-type-${pair_index}" required value="${truthType}"/>
+                    <div class="object-pair-label reference-forecast-name-${pair_index}"><b>Reference Forecast: </b> ${ref_fxName}</div>
+                    <input type="hidden" class="form-control reference-forecast-value" name="reference-forecast-${pair_index}" required value="${ref_fxId}"/>
                   </div>
                  </div>
-                 <a role="button" class="object-pair-delete-button">x</a>
+                 <a role="button" class="object-pair-delete-button">remove</a>
                </div>`);
         var remove_button = new_object_pair.find(".object-pair-delete-button");
         remove_button.click(function(){
             new_object_pair.remove();
             if ($('.object-pair-list .object-pair').length == 0){
                 $('.empty-reports-list')[0].hidden = false;
+                unset_units();
             }
         });
         return new_object_pair;
     }
 
 
-    function newSelector(field_type, depends_on=null){
+    function newSelector(field_name, depends_on=null, required=true){
         /*
          * Returns a JQuery object containing labels and select elements for appending options to.
          * Initializes with one default and one optional select option:
          *     Always adds an option containing "No matching <field_Type>s
          *     If depends_on is provided, inserts a "Please select a <depends_on> option>
          */
+        var field_type = field_name.toLowerCase().replace(' ', '-');
         return $(`<div class="form-element full-width ${field_type}-select-wrapper">
-                    <label>Select a ${field_type}</label>
-                      <div class="report-field-filters"><input id="${field_type}-option-search" class="form-control half-width" placeholder="Search by ${field_type} name"/></div><br>
+                    <label>Select a ${field_name} ${required ? "" : "(Optional)"}</label>
+                      <div class="report-field-filters"><input id="${field_type}-option-search" class="form-control half-width" placeholder="Search by ${field_name} name"/></div><br>
                     <div class="input-wrapper">
                       <select id="${field_type}-select" class="form-control ${field_type}-field" name="${field_type}-select" size="5">
                       ${depends_on ? `<option id="no-${field_type}-${depends_on}-selection" disabled> Please select a ${depends_on}.</option>` : ""}
@@ -225,10 +286,76 @@ $(document).ready(function() {
             } else {
                 $('#no-forecasts').attr('hidden', true);
             }
+            filterReferenceForecasts(variable);
             if (compareTo == 'observation'){
                 filterObservations();
             } else {
                 filterAggregates();
+            }
+        }
+
+        function filterReferenceForecasts(variable){
+            forecast = forecast_select.find(':selected').first();
+            reference_forecasts = $('#reference-forecast-select option').slice(2);
+            reference_forecasts.removeAttr('hidden');
+            if (forecast[0]){
+                console.log("selected forecast ", forecast);
+                if(forecast.data.hasOwnProperty('siteId')){
+                    site_id = forecast.data().siteId;
+                }else{
+                    aggregate_id = forecast.data().aggregateId;
+                }
+                variable = forecast.data().variable;
+                console.log('selected variable ', variable);
+                interval_length = forecast.data().intervalLength;
+                console.log('selected il ', interval_length);
+                // hide the "please select forecast" prompt"
+                $('#no-reference-forecast-forecast-selection').attr('hidden', true);
+                toHide = searchSelect('#reference-forecast-option-search',
+                                  '#reference-forecast-select', 2);
+                if (variable){
+                toHide = toHide.add(reference_forecasts.not(
+                    `[data-variable=${variable}]`));
+                }
+                console.log("after var", toHide);
+                // Determine if we need to filter by site or aggregate
+                if (site_id){
+                    // create a set of elements to hide from selected site, variable and search
+                    toHide = toHide.add(reference_forecasts.not(
+                        `[data-site-id=${site_id}]`));
+                } else {
+                    toHide = toHide.add(
+                        reference_forecasts.not(
+                            `[data-aggregate-id=${aggregate_id}]`));
+                }
+                // Filter out reference forecasts that don't have the same
+                // interval length
+                things = reference_forecasts.filter(function(){
+                    return $(this).data().intervalLength != interval_length ||
+                        $(this).attr('value') == forecast_select.val();
+                });
+                toHide = toHide.add(things);
+            }else{
+                toHide = reference_forecasts;
+                $('#no-reference-forecast-forecast-selection').removeAttr('hidden');
+                console.log('no forecast');
+            }
+            
+            console.log("after search", toHide);
+            
+            // if current forecast selection is invalid, deselect
+            if (toHide.filter(':selected').length){
+                ref_forecast_select.val('');
+            }
+            toHide.attr('hidden', 'true');
+            // if all options are hidden, show "no matching forecasts"
+            if (toHide.length == reference_forecasts.length){
+                ref_forecast_select.val('');
+                if ($('#no-reference-forecast-site-selection').attr('hidden') || compareTo == 'aggregate'){
+                    $('#no-reference-forecasts').removeAttr('hidden');
+                }
+            } else {
+                $('#no-reference-forecasts').attr('hidden', true);
             }
         }
 
@@ -304,6 +431,7 @@ $(document).ready(function() {
         var siteSelector = newSelector("site");
         var obsSelector = newSelector("observation", "forecast");
         var fxSelector = newSelector("forecast", "site");
+        var refFxSelector = newSelector("reference forecast", "forecast", required=false);
         var fxVariableSelector = createVariableSelect();
         fxSelector.find('.report-field-filters').append(fxVariableSelector);
         var addObsButton = $('<a role="button" class="btn btn-primary" id="add-obs-object-pair" style="padding-left: 1em">Add a Forecast, Observation pair</a>');
@@ -322,6 +450,7 @@ $(document).ready(function() {
         // be inserted into the DOM
         widgetContainer.append(siteSelector);
         widgetContainer.append(fxSelector);
+        widgetContainer.append(refFxSelector);
         widgetContainer.append(obsSelector);
         widgetContainer.append(aggregateSelector);
         widgetContainer.append(addObsButton);
@@ -335,18 +464,22 @@ $(document).ready(function() {
         siteSelector.find('#site-option-search').keyup(filterSites);
         obsSelector.find('#observation-option-search').keyup(filterObservations);
         fxSelector.find('#forecast-option-search').keyup(filterForecasts);
+        fxSelector.find('#reference-forecast-option-search').keyup(filterForecasts);
         aggregateSelector.find('#aggregate-option-search').keyup(filterAggregates);
 
         // create variables pointing to the specific select elements
         var observation_select = obsSelector.find('#observation-select');
         var forecast_select = fxSelector.find('#forecast-select');
+        var ref_forecast_select = refFxSelector.find('#reference-forecast-select');
         var site_select = siteSelector.find('#site-select');
         var aggregate_select = aggregateSelector.find('#aggregate-select');
         
+        // set callbacks for select inputs
         site_select.change(filterForecasts);
         variable_select.change(filterForecasts);
         forecast_select.change(filterObservations);
         forecast_select.change(filterAggregates);
+        forecast_select.change(filterReferenceForecasts);
 
         // insert options from page_data into the select elements
         $.each(page_data['sites'], function(){
@@ -367,8 +500,17 @@ $(document).ready(function() {
                     .attr('data-variable', this.variable));
         });
         $.each(page_data['forecasts'], function(){
-            forecast_select.append(
-                $('<option></option>')
+            forecast_select.append($('<option></option>')
+                    .html(this.name)
+                    .val(this.forecast_id)
+                    .attr('hidden', true)
+                    .attr('data-site-id', this.site_id)
+                    .attr('data-aggregate-id', this.aggregate_id)
+                    .attr('data-interval-length', this.interval_length)
+                    .attr('data-variable', this.variable));
+        });
+        $.each(page_data['forecasts'], function(){
+            ref_forecast_select.append($('<option></option>')
                     .html(this.name)
                     .val(this.forecast_id)
                     .attr('hidden', true)
@@ -399,15 +541,28 @@ $(document).ready(function() {
                 // If both inputs contain valid data, create a pair and add it to the DOM
                 var selected_observation = observation_select.find('option:selected')[0];
                 var selected_forecast = forecast_select.find('option:selected')[0];
+                var selected_reference_forecast = ref_forecast_select.find('option:selected')[0];
+                if(!selected_reference_forecast){
+                    ref_text = "Unset";
+                    ref_id = null;
+                }else{
+                    ref_text = selected_reference_forecast.text;
+                    ref_id = selected_reference_forecast.value;
+                }
+
                 pair = addPair('observation',
                                selected_observation.text,
                                selected_observation.value,
                                selected_forecast.text,
-                               selected_forecast.value);
-
+                               selected_forecast.value,
+                               ref_text,
+                               ref_id,
+                );
                 pair_container.append(pair);
                 pair_index++;
-                $(".empty-reports-list")[0].hidden = true;
+                var variable = selected_forecast.dataset.variable;
+                set_units(variable);
+                $(".empty-reports-list").attr('hidden', 'hidden');
                 forecast_select.css('border', '');
                 observation_select.css('border', '');
             } else {
@@ -427,13 +582,27 @@ $(document).ready(function() {
             if (aggregate_select.val() && forecast_select.val()){
                 var selected_aggregate = aggregate_select.find('option:selected')[0];
                 var selected_forecast = forecast_select.find('option:selected')[0];
+                var selected_reference_forecast = ref_forecast_select.find('option:selected')[0];
+                if(!selected_reference_forecast){
+                    ref_text = "Unset";
+                    ref_id = null;
+                }else{
+                    ref_text = selected_reference_forecast.text;
+                    ref_id = selected_reference_forecast.value;
+                }
                 pair = addPair('aggregate',
                                selected_aggregate.text,
                                selected_aggregate.value,
                                selected_forecast.text,
-                               selected_forecast.value);
+                               selected_forecast.value,
+                               ref_text,
+                               ref_id,
+                );
                 pair_container.append(pair);
                 pair_index++;
+                var variable = selected_forecast.dataset.variable;
+                set_units(variable);
+
                 $(".empty-reports-list")[0].hidden = true;
                 forecast_select.css('border', '');
                 observation_select.css('border', '');
@@ -467,3 +636,33 @@ $(document).ready(function() {
     registerDatetimeValidator('period-start');
     registerDatetimeValidator('period-end')
 });
+
+
+function insertErrorMessage(title, msg){
+    $('#form-errors').append(`<li class="alert alert-danger"><p><b>${title}: </b>${msg}</p></li>`);
+}
+function validateReport(){
+    /*
+     * Callback before the report form is submitted. Any js validation should
+     * occur here.
+     */
+    // remove any existing errors
+    $('#form-errors').empty();
+
+    // Assert that if skill is selected, all pairs have a reference forecast.
+    selected_metrics = $('input[name="metrics"]').map(function(){return this.value}).get();
+    reference_forecasts = $('.reference-forecast-value').map(function(){return this.value}).get();
+    if(selected_metrics.includes("s") && reference_forecasts.some((x) => x != 'Unset')){
+        insertErrorMessage(
+            "Skill Metric",
+            "Requires that all Observation, Forecast pairs have a reference forecast.");
+    }
+
+    // asset at least one pair was selected.
+    if($('.object-pair').length == 0){
+        insertErrorMessage(
+            "Analysis Pairs",
+            "Must specify at least one Observation, Forecast pair.");
+    }
+    return false;
+}
