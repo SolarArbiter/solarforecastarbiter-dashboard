3c746f4e1c6e34268362e7d82efc4aa649b429eb
Leland Boeman <lboeman@email.arizona.edu>
2020-06-04 17:12:02 -0700

    add explicit check tests for each stage of site conversion

--- sfa_dash/form_utils/tests/test_converter.py
+++ sfa_dash/form_utils/tests/test_converter.py
@@ -39,12 +39,71 @@ def test_site_converter_roundtrip(site_with_modeling_params):
     assert api_data == expected
 
 
-def test_site_converter_payload_to_formdata():
-    pass
+def test_site_converter_payload_to_formdata(site):
+    form_data = converters.SiteConverter.payload_to_formdata(site)
+    assert form_data['name'] == 'Weather Station'
+    assert form_data['elevation'] == 595.0
+    assert form_data['latitude'] == 42.19
+    assert form_data['longitude'] == -122.7
+    assert form_data['timezone'] == 'Etc/GMT+8'
+    assert form_data['site_type'] == 'weather-station'
 
 
-def test_site_converter_formdata_to_payload():
-    pass
+def test_site_converter_payload_to_formdata_plant(site_with_modeling_params):
+    form_data = converters.SiteConverter.payload_to_formdata(
+        site_with_modeling_params)
+    assert form_data['name'] == 'Power Plant 1'
+    assert form_data['elevation'] == 786.0
+    assert form_data['latitude'] == 43.73403
+    assert form_data['longitude'] == -96.62328
+    assert form_data['timezone'] == 'Etc/GMT+6'
+    assert form_data['site_type'] == 'power-plant'
+    assert form_data['ac_capacity'] == 0.015
+    assert form_data['ac_loss_factor'] == 0.0
+    assert form_data['dc_capacity'] == 0.015
+    assert form_data['dc_loss_factor'] == 0.0
+    assert form_data['surface_azimuth'] == 180.0
+    assert form_data['surface_tilt'] == 45.0
+    assert form_data['temperature_coefficient'] == -0.2
+    assert form_data['tracking_type'] == 'fixed'
+
+
+def test_site_converter_formdata_to_payload(site_with_modeling_params):
+    form_data = {
+        'name': 'Power Plant 1',
+        'elevation': 786.0,
+        'latitude': 43.73403,
+        'longitude': -96.62328,
+        'timezone': 'Etc/GMT+6',
+        'site_type': 'power-plant',
+        'ac_capacity': 0.015,
+        'ac_loss_factor': 0.0,
+        'dc_capacity': 0.015,
+        'dc_loss_factor': 0.0,
+        'surface_azimuth': 180.0,
+        'surface_tilt': 45.0,
+        'temperature_coefficient': -0.2,
+        'tracking_type': 'fixed',
+    }
+    api_payload = converters.SiteConverter.formdata_to_payload(form_data)
+    assert api_payload == {
+        'name': 'Power Plant 1',
+        'elevation': 786.0,
+        'latitude': 43.73403,
+        'longitude': -96.62328,
+        'timezone': 'Etc/GMT+6',
+        'modeling_parameters': {
+            'ac_capacity': 0.015,
+            'ac_loss_factor': 0.0,
+            'dc_capacity': 0.015,
+            'dc_loss_factor': 0.0,
+            'surface_azimuth': 180.0,
+            'surface_tilt': 45.0,
+            'temperature_coefficient': -0.2,
+            'tracking_type': 'fixed',
+        },
+    }
+
 
 
 def test_site_observation_converter_roundtrip(observation):
