007d7bb7b61465ffe3b0faba67f8628e3dccdd69
Leland Boeman <lboeman@email.arizona.edu>
2020-11-12 11:17:24 -0800

    copy uuid button, fix report listing popups (#383)

--- sfa_dash/static/js/report-listing.js
+++ sfa_dash/static/js/report-listing.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
     $('.report-details-expander').click(function(){
+        // Close any other open report details
+        $('div.report-metadata-popup[hidden!=hidden]').attr('hidden', true);
         $(this).next().removeAttr('hidden');
     });
     $('.report-details-closer').click(function(){
         $(this).parent().attr('hidden', true);
     });
 })
+$(document).click(function(event){
+    let target = $(event.target);
+    // first check that the user didn't click the expander button
+    if (!target.closest('.report-details-expander').length){
+        // if there is no popup in the target, close all popups.
+        if (!target.closest('div.report-metadata-popup').length){
+            $('div.report-metadata-popup[hidden!=hidden]').attr('hidden', true);
+        }
+    }
+
+});
